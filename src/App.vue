180e35257df259e7b17337a39c3b7367e906a567
chzzy <chzzytool@163.com>
2021-04-14 17:08:35 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,20 @@
 </template>
 
 <style lang="less">
+html,
+body {
+  padding: 0;
+  margin: 0;
+  height: 100%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100%;
+  height: 100%;
 }
 
 #nav {
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div id="mapDiv"></div>
 </template>
+
+<script>
+import "ol/ol.css";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+export default {
+  mounted() {
+    new Map({
+      target: "mapDiv",
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 0,
+      }),
+    });
+  },
+};
+</script>
+
+<style scoped>
+#mapDiv {
+  width: 100%;
+  height: 100%;
+}
+</style>
