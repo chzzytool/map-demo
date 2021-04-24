96de752a2adc9a8e8a96d5c5edd610b3b1a17382
chzzy <chinesezzy@163.com>
2021-04-24 10:39:28 +0800

    添加页面元素

--- src/views/Demo1.vue
+++ src/views/Demo1.vue
@@ -2,18 +2,72 @@
   <div id="container">
     <div id="map"></div>
     <div class="search-box">
-      <input class="search" />
+      <input v-model="searchKey" @change="handleSearchChange" class="search" />
     </div>
-    <!-- <div class="layer-box">
-      <span class="layer-title">地表覆盖类型</span>
-      <input type="radio" name="layerType" value="1" />水体
-      <input type="radio" name="layerType" value="2" />湿地
-      <input type="radio" name="layerType" value="3" />耕地
-      <input type="radio" name="layerType" value="4" />森林
-      <input type="radio" name="layerType" value="5" />草地
-    </div> -->
-    <div ref="info" class="info-box" v-if="info.name">
+    <div class="layer-box">
+      <div class="layer">
+        <span class="layer-title">地表覆盖类型</span>
+        <div class="radio-item">
+          <input
+            type="radio"
+            class="radio-circle"
+            v-model="layerTypeValue"
+            name="layerType"
+            value="1"
+          />
+          <span class="radio-label">水体 </span>
+        </div>
+
+        <div class="radio-item">
+          <input
+            type="radio"
+            class="radio-circle"
+            v-model="layerTypeValue"
+            name="layerType"
+            value="2"
+          />
+          <span class="radio-label">湿地</span>
+        </div>
+
+        <div class="radio-item">
+          <input
+            type="radio"
+            class="radio-circle"
+            v-model="layerTypeValue"
+            name="layerType"
+            value="3"
+          />
+          <span class="radio-label">耕地</span>
+        </div>
+
+        <div class="radio-item">
+          <input
+            type="radio"
+            class="radio-circle"
+            v-model="layerTypeValue"
+            name="layerType"
+            value="4"
+          />
+          <span class="radio-label">森林</span>
+        </div>
+
+        <div class="radio-item">
+          <input
+            type="radio"
+            class="radio-circle"
+            v-model="layerTypeValue"
+            name="layerType"
+            value="5"
+          />
+          <span class="radio-label">草地</span>
+        </div>
+      </div>
+    </div>
+    <!-- <div ref="info" class="info-box" v-if="info.name"> -->
+    <div ref="info" class="info-box">
       <span class="info1">省名称：{{ info.name }} </span>
+      <span class="info1">搜索：{{ searchKey }} </span>
+      <span class="info1">图层：{{ layerTypeValue }} </span>
     </div>
   </div>
 </template>
@@ -31,6 +85,8 @@ import * as olProj from "ol/proj";
 export default {
   data() {
     return {
+      searchKey: "", //搜索关键字
+      layerTypeValue: "", //图层选择值
       info: {
         name: "",
       }, //描述信息
@@ -61,7 +117,8 @@ export default {
 
       const vectorLayer = new VectorLayer({
         source: new VectorSource({
-          url: "https://geo.datav.aliyun.com/areas_v2/bound/100000_full.json",
+          // url: "https://geo.datav.aliyun.com/areas_v2/bound/100000_full.json",
+          url: "https://geo.datav.aliyun.com/areas_v2/bound/100000.json",
           format: new GeoJSON(),
         }),
         style: function (feature) {
@@ -146,31 +203,36 @@ export default {
       map.on("click", function (evt) {
         displayFeatureInfo(evt.pixel);
         console.log("evt-->", evt);
+        var pixel = map.getEventPixel(evt.originalEvent);
+        map.forEachFeatureAtPixel(pixel, function (feature) {
+          if (feature != undefined) {
+            console.log(feature);
+          }
+        });
       });
 
       map.on("singleclick", function (e) {
         console.log("e->", e);
-        console.log(
-          "经纬度1-->",
-          olProj.transform(e.coordinate, "EPSG:3857", "EPSG:4326")
+        const arr1 = olProj.transform(e.coordinate, "EPSG:3857", "EPSG:4326");
+        const arr2 = olProj.transform(
+          map.getEventCoordinate(e.originalEvent),
+          "EPSG:3857",
+          "EPSG:4326"
         );
-        console.log(
-          "经纬度2-->",
-          olProj.transform(
-            map.getEventCoordinate(e.originalEvent),
-            "EPSG:3857",
-            "EPSG:4326"
-          )
+        const arr3 = olProj.transform(
+          map.getCoordinateFromPixel(e.pixel),
+          "EPSG:3857",
+          "EPSG:4326"
         );
-        console.log(
-          "经纬度3-->",
-          olProj.transform(
-            map.getCoordinateFromPixel(e.pixel),
-            "EPSG:3857",
-            "EPSG:4326"
-          )
-        );
+        console.log("经纬度1-->", arr1);
+        console.log("经纬度2-->", arr2);
+        console.log("经纬度3-->", arr3);
       });
+    },
+
+    handleSearchChange(e) {
+      const value = e.target.value;
+      console.log("value->", value);
     },
   },
   mounted() {
@@ -193,11 +255,59 @@ export default {
   left: 100px;
   top: 12px;
 }
+.search {
+  padding: 4px 4px 5px 6px;
+  font-size: 14px;
+  border-radius: 2px;
+  border: 1px solid #ccc;
+  outline: 0;
+}
+.search:focus {
+  border: 1px solid #aaa;
+}
 .layer-box {
   position: absolute;
-  right: 100px;
+  right: 12px;
   top: 12px;
+  width: 120px;
+  background: #d3d7d4;
+  font-size: 14px;
+  border-radius: 2px;
 }
+.layer {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+
+.layer-title {
+  text-align: left;
+  padding-left: 6px;
+}
+
+.radio-item {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+}
+
+.radio-circle {
+  font-size: 14px;
+  /* width: 20px;
+  height: 20px; */
+  padding: 0;
+  background-color: #fff;
+  border: 1px solid #c9c9c9;
+  border-radius: 50%;
+  outline: none;
+  margin-right: 22px;
+  cursor: pointer;
+  background-color: indianred;
+}
+
+.radio-circle:checked {
+}
+
 .info-box {
   position: absolute;
   right: 12px;
